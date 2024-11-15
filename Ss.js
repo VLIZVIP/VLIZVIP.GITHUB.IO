2f45b3c2d99033101c965c902e1abe156adcdba1
VLIZVIP <162181304+VLIZVIP@users.noreply.github.com>
2024-11-15 05:45:40 +0100

    Update Ss.js

--- Ss.js
+++ Ss.js
@@ -2,13 +2,13 @@
 
 loopy() 
  function loopy() { 
- var sWord ="mrwanxX" 
+ var sWord ="Dev.Vliz" 
  while (sWord != "VLIZ") {// Pass
- sWord = prompt("𝘌𝘯𝘵𝘦𝘳 𝘛𝘩𝘦 𝘒𝘦𝘺 :")  
+ sWord = prompt("𝐂𝐨𝐧𝐭𝐚𝐜𝐭𝐨 𝐕𝐥𝐢𝐳_𝐯𝐢𝐩 +𝟏 𝟖𝟐𝟗 𝟔𝟗𝟐 𝟐𝟎𝟒𝟎 𝘌𝘯𝘵𝘦𝘳 𝘛𝘩𝘦 𝘒𝘦𝘺 :")  
  } 
  
 //confirm("ok");
 
-window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 HAX STAR IOS ");
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 𝑽𝒍𝒊𝒛_𝒗𝒊𝒑 𝒊𝒐𝒔 ");
 
  } 
