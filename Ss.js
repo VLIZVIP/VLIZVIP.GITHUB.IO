7e0fc52a75269af38237204a590208db583b6a6e
VLIZVIP <162181304+VLIZVIP@users.noreply.github.com>
2024-11-15 06:29:52 +0100

    Update Ss.js

--- Ss.js
+++ Ss.js
@@ -4,7 +4,7 @@ loopy()
  function loopy() { 
  var sWord ="Dev.Vliz" 
  while (sWord != "VLIZ") {// Pass
- sWord = prompt("𝐂𝐨𝐧𝐭𝐚𝐜𝐭𝐨 𝐕𝐥𝐢𝐳_𝐯𝐢𝐩 +𝟏 𝟖𝟐𝟗 𝟔𝟗𝟐 𝟐𝟎𝟒𝟎 𝘌𝘯𝘵𝘦𝘳 𝘛𝘩𝘦 𝘒𝘦𝘺 :")  
+ sWord = prompt("𝐂𝐨𝐧𝐭𝐚𝐜𝐭𝐨 𝐕𝐥𝐢𝐳_𝐯𝐢𝐩 +𝟏 𝟖49 515 4002 𝘌𝘯𝘵𝘦𝘳 𝘛𝘩𝘦 𝘒𝘦𝘺 :")  
  } 
  
 //confirm("ok");
